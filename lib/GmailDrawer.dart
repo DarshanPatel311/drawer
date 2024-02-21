dc442b10ffcee2d2ed3b3988fc8632610bb4060b
~JAY GOGA~ <dkpatel3116@gmail.com>
2024-02-21 22:27:51 +0530

    21-2-24

--- lib/GmailDrawer.dart
+++ lib/GmailDrawer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 
 class Gmail extends StatefulWidget {
   const Gmail({super.key});
@@ -8,24 +8,203 @@ class Gmail extends StatefulWidget {
   @override
   State<Gmail> createState() => _GmailState();
 }
-
+ late double height;
+late double width;
 class _GmailState extends State<Gmail> {
   @override
   Widget build(BuildContext context) {
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
     return MaterialApp(
 debugShowCheckedModeBanner: false,
 
       home: SafeArea(
 
         child: Scaffold(
-          drawer: Drawer(
+          drawer: Container(
+            height: height,
             width: 300,
-            backgroundColor: Color(0xff2E2F33),
-          clipBehavior: Clipboard.,
+            color: Color(0xff121315),
             child: Column(
+              children: [
+                Container(
+                  height: 30,
 
-                children: [
-                  Icon(Icons.all_inbox),
+                ),
+                 Row(
+                   children: [
+
+                     Padding(
+                       padding: const EdgeInsets.symmetric(horizontal: 15,),
+                       child: Text("Gmail",style: TextStyle(
+                         color: Colors.white,
+                         fontSize: 23,
+                         fontWeight: FontWeight.bold,
+                       ),),
+                     ),
+                   ],
+                 ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 18),
+                  child: Row(
+                    children: [
+                      Icon(Icons.all_inbox,color: Colors.white,size: 25,),
+                      Text("         All inboxes",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 18),
+                  child: Row(
+                    children: [
+                      Icon(Icons.inbox,color: Colors.white,size: 25,),
+                      Text("          Inboxes",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.star_border,color: Colors.white,size: 25,),
+                      Text("          Starred",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.access_time_outlined,color: Colors.white,size: 25,),
+                      Text("          Snoozed",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.label_important_outline_rounded,color: Colors.white,size: 25,),
+                      Text("          Important",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.send_outlined,color: Colors.white,size: 25,),
+                      Text("          Sent",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.description_rounded,color: Colors.white,size: 25,),
+                      Text("          Drafts",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.mail_outline,color: Colors.white,size: 25,),
+                      Text("          All Mail",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.report_gmailerrorred,color: Colors.white,size: 25,),
+                      Text("          Spam",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.delete_sweep_outlined,color: Colors.white,size: 25,),
+                      Text("          Trash",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 18),
+                  child: Row(
+                    children: [
+                      Icon(Icons.add,color: Colors.white,size: 25,),
+                      Text("          Create new",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+                  child: Row(
+                    children: [
+                      Icon(Icons.settings_outlined,color: Colors.white,size: 25,),
+                      Text("          Setting",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+
+                      ),),
+                    ],
+                  ),
+                ),
                 ],
               ),
           ),
